de33812e513df563a15cd178e4c93f0b1128eb24
Augusto Almeida <augusto.nascimentto@gmail.com>
2025-10-04 01:20:27 +0000

    Atualizado: Melhorias e correções

--- iac1.sh
+++ iac1.sh
@@ -2,9 +2,7 @@
 
 echo "Instalando o Figlet" 
 
-sudo apt-get install figlet
-
-touch /etc/skel/bemvindo.txt
+apt-get update -y && apt-get install -y figlet
 
 echo "Olá, me chamo Augusto e fiz a criação do Script, lembre-se de alterar sua senha" > /etc/skel/bemvindo.txt
 
@@ -14,41 +12,43 @@ echo "Começando o Script..."
 
 echo "Criando os diretórios..."
 
-mkdir /producao
-mkdir /desevolvimento
-mkdir /teste
+mkdir -p /producao /desenvolvimento /teste
 
 echo "Criando os Grupos..." 
 
-groupadd gproducao
-groupadd gdesenvolvimento
-groupadd gteste
+groupadd -f gproducao
+groupadd -f gdesenvolvimento
+groupadd -f gteste
 
 echo "Criando os usuários..." 
 
-useradd uprod -m -c "Usuário para PROD" -p $(openssl passwd -crypt 123) -s /bin/bash -G gproducao 
-useradd udesen -m -c "Usuário para DEV" -p $(openssl passwd -crypt 123) -s /bin/bash -G gdesenvolvimento
-useradd utest -m -c "Usuário para Test" -p $(openssl passwd -crypt 123) -s /bin/bash -G gteste 
+useradd -m -c "Novo usuário" -p $(openssl passwd -6 Senha123) -s /bin/bash -G gproducao uprod 
+useradd -m -c "Novo usuário" -p $(openssl passwd -6 Senha123) -s /bin/bash -G gdesenvolvimento udesen
+useradd -m -c "Novo usuário" -p $(openssl passwd -6 Senha123) -s /bin/bash -G gteste utest
 
 echo "Criando as regras de grupos para diretórios"
 
-chown root:uprod /producao
-chown root:udesen /desenvolvimento
-chown root:utest /teste
+chown root:gproducao /producao
+chown root:gdesenvolvimento /desenvolvimento
+chown root:gteste /teste
 
 chmod 770 /producao
 chmod 770 /desenvolvimento
 chmod 770 /teste
 
-figlet Fim do Script.
+echo "Gerando Logs para usuários e grupos"
 
+grep "uprod" /etc/passwd > log_users.txt
+grep "gproducao" /etc/group > log_groups.txt
 
-cat /etc/passwd | grep "uprod" > users.txt && cat /etc/group | grep "gproducao" > group.txt
-cat /etc/passwd | grep "udesen" >> users.txt && cat /etc/group | grep "gdesenvolvimento" >> group.txt
-cat /etc/passwd | grep "utest" >> users.txt && cat /etc/group | grep "gteste" >> group.txt
+grep "udesen" /etc/passwd >> log_users.txt
+grep "gdesenvolvimento" /etc/group >> log_groups.txt
 
+grep "utest" /etc/passwd >>  log_users.txt
+grep "gteste" /etc/group  >> log_groups.txt
 
 echo "____________________________________________"
 
+figlet Script concluido
 
 
